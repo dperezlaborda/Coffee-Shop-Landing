65a979a8b6e316df7546e4ffd7109f421020f5f1
dolores <dperezlaborda@gmail.com>
2020-02-24 16:19:41 -0300

    terminado el footer

--- src/Sections/8-Eight/SectionEight.js
+++ src/Sections/8-Eight/SectionEight.js
@@ -5,21 +5,21 @@ const SectionEight = () =>{
     return(
         <footer className="container-footer">
             <div className="half footer-left">
-                <h2>COME ON IN!</h2>
-                <h3>WEEKDAYS</h3>
-                <p>08pm - 12am</p>
-                <h3>WEEKENDS</h3>
-                <p>12pm - 02am</p>
+                <h2 className="footer-title">COME ON IN!</h2>
+                <h3 className="footer-sub-t">WEEKDAYS</h3>
+                <p className="footer-text">08pm - 12am</p>
+                <h3 className="footer-sub-t">WEEKENDS</h3>
+                <p className="footer-text">12pm - 02am</p>
                 <h4>BROWSE MENU</h4>
             </div>
             <div className="half footer-right">
-                <h2>CONTACT</h2>
-                <h3>PHONE</h3>
-                <p>(011) 4331 2200</p>
-                <h3>MAIL</h3>
-                <p>dperezlaborda@gmail.com</p>
-                <h3>ADDRESS</h3>
-                <p>171 Lake Washington Blvd, Seattle</p>
+                <h2 className="footer-title">CONTACT</h2>
+                <h3 className="footer-sub-t">PHONE</h3>
+                <p className="footer-text">(011) 4331 2200</p>
+                <h3 className="footer-sub-t">MAIL</h3>
+                <p className="footer-text">dperezlaborda@gmail.com</p>
+                <h3 className="footer-sub-t">ADDRESS</h3>
+                <p className="footer-text">171 Lake Washington Blvd, Seattle</p>
             </div>
         </footer>
     );
